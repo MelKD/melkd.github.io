67c401d6adf3b5dcce35a59a44afeb836a4e5458
Melissa Kyd <mel_kyd@hotmail.com>
2020-09-27 14:54:22 +0930

    Commit transferred files for personal website

---
+++ more-doggos/precache-manifest.97ff399718d2bdb626392578e1e6f23a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cac80a6673e2cf0ea684a5540e3f5f7",
+    "url": "/more-doggos/index.html"
+  },
+  {
+    "revision": "1adcd82099288a9c0033",
+    "url": "/more-doggos/static/css/main.36266253.chunk.css"
+  },
+  {
+    "revision": "746f5f288788b300d071",
+    "url": "/more-doggos/static/js/2.1f6dbe34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/more-doggos/static/js/2.1f6dbe34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adcd82099288a9c0033",
+    "url": "/more-doggos/static/js/main.f7d21fad.chunk.js"
+  },
+  {
+    "revision": "9a311f1d58544136b0e3",
+    "url": "/more-doggos/static/js/runtime-main.20fc4628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/more-doggos/static/media/logo.5d5d9eef.svg"
+  }
+]);
